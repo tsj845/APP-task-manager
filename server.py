1aa4b9471f8c1b40d9f70d49c4d3b5413fa97f72
Vihaan Jim <vihaan842@users.noreply.github.com>
2021-12-09 17:05:30 -0500

    change priorities to stars instead of sections

--- server.py
+++ server.py
@@ -16,18 +16,34 @@ server.add_url_rule("/projects", endpoint="index")
 def useful_functions():
         def task_html(task):
                 if "subtasks" in task:
-                        html = "<p class=\'task\'>" + task["disp-value"] + "</p>" + "<ul>"
+                        html = "<p class=\'task\'>" + task["disp-value"]
+                        if "labels" in task:
+                                if "important" in task["labels"]:
+                                        html += " ★"
+                                elif "veryimportant" in task["labels"]:
+                                        html += " ★★"
+                                elif "urgent" in task["labels"]:
+                                        html += " ★★★"
+                        html += "</p>" + "<ul>"
                         for subtask in task["subtasks"]:
                                 html += "<li>" + task_html(subtask) + "</li>"
                         html += "</ul>"
                         return html
                 else:
-                        return "<p class=\'task\'>" + task["disp-value"] + "</p>"
+                        html = "<p class=\'task\'>" + task["disp-value"]
+                        if "labels" in task:                        
+                                if "important" in task["labels"]:
+                                        html += " ★"
+                                elif "veryimportant" in task["labels"]:
+                                        html += " ★★"
+                                elif "urgent" in task["labels"]:
+                                        html += " ★★★"         
+                        return html + "</p>"
         return dict(task_html=task_html)
         
 @server.route("/projects/<project>")
 def project (project):
-	tasks = {"low":[{"disp-value":"task1", "labels":[], "subtasks":[{"disp-value":"subtask1"}, {"disp-value":"subtask2", "subtasks":[{"disp-value":"subsubtask1"}]}]}, {"disp-value":"task2", "labels":[]}, {"disp-value":"task3", "labels":[]}], "med":[{"disp-value":"task4", "labels":[]}], "high":[{"disp-value":"task5", "labels":[]}], "top-e":True, "top":{"disp-value":"task6", "labels":["top"]}}
+	tasks = [{"disp-value":"task1", "labels":[], "subtasks":[{"disp-value":"subtask1"}, {"disp-value":"subtask2", "subtasks":[{"disp-value":"subsubtask1"}]}]}, {"disp-value":"task2", "labels":[]}, {"disp-value":"task3", "labels":[]}, {"disp-value":"task4", "labels":["important"]}, {"disp-value":"task5", "labels":["veryimportant"]}, {"disp-value":"task6", "labels":["urgent"]}]
 	return render("manager-template.html", project_name=project, project=tasks)
 
 server.run(host="127.0.0.1", port="3000", debug=True)
