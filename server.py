0ddf99b933bdb899b53fb81b9f082311e3f0a94c
Tristan <73963573+tsj845@users.noreply.github.com>
2021-12-09 15:40:54 +0000

    fixed redirect

--- server.py
+++ server.py
@@ -5,14 +5,16 @@ server = Flask(__name__)
 server.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @server.route("/")
-def projects():
+def projects ():
 	projects = ["project1", "project2", "project3"]
 	return render("projects-template.html", projects=projects)
 
+@server.route("/projects")
+def project ():
+	return server.redirect("/")
+
 @server.route("/projects/<project>")
-def project(project):
-	if (project == ""):
-		return server.redirect("/")
+def project (project):
 	tasks = {"low":[{"disp-value":"task1"}, {"disp-value":"task2"}, {"disp-value":"task3"}], "med":[{"disp-value":"task4"}], "high":[{"disp-value":"task5"}], "top-e":True, "top":{"disp-value":"task6"}}
 	return render("manager-template.html", project_name=project, project=tasks)
 
