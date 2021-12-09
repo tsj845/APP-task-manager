edc1338991ef0e59878ab72f9feed724dd58c497
Vihaan Jim <vihaan842@users.noreply.github.com>
2021-12-08 19:50:26 -0500

    projects page

--- server.py
+++ server.py
@@ -2,9 +2,16 @@ from flask import Flask
 from flask import render_template as render
 
 server = Flask(__name__)
+server.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @server.route("/")
-def hello_world():
-    return "<h1>hello world</h1>"
+def projects():
+    projects = ["project1", "project2", "project3"]
+    return render("projects-template.html", projects=projects)
+
+@server.route("/<project>")
+def project(project):
+    tasks = ["task1", "task2", "task3"]
+    return render("manager-template.html", project_name=project, project=tasks)
 
 server.run(host="127.0.0.1", port="3000", debug=True)
