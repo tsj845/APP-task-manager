8f6f229625ee42dd7236e72ab6bd3d677d617b8a
Vihaan Jim <vihaan842@users.noreply.github.com>
2021-12-09 15:42:39 -0500

    add subtasks

--- server.py
+++ server.py
@@ -12,9 +12,22 @@ def projects ():
 server.add_url_rule("/", endpoint="index")
 server.add_url_rule("/projects", endpoint="index")
 
+@server.context_processor
+def useful_functions():
+        def task_html(task):
+                if "subtasks" in task:
+                        html = task["disp-value"] + "<ul>"
+                        for subtask in task["subtasks"]:
+                                html += "<li>" + task_html(subtask) + "</li>"
+                        html += "</ul>"
+                        return html
+                else:
+                        return task["disp-value"]
+        return dict(task_html=task_html)
+        
 @server.route("/projects/<project>")
 def project (project):
-	tasks = {"low":[{"disp-value":"task1", "labels":[]}, {"disp-value":"task2", "labels":[]}, {"disp-value":"task3", "labels":[]}], "med":[{"disp-value":"task4", "labels":[]}], "high":[{"disp-value":"task5", "labels":[]}], "top-e":True, "top":{"disp-value":"task6", "labels":["top"]}}
+	tasks = {"low":[{"disp-value":"task1", "labels":[], "subtasks":[{"disp-value":"subtask1"}, {"disp-value":"subtask2", "subtasks":[{"disp-value":"subsubtask1"}]}]}, {"disp-value":"task2", "labels":[]}, {"disp-value":"task3", "labels":[]}], "med":[{"disp-value":"task4", "labels":[]}], "high":[{"disp-value":"task5", "labels":[]}], "top-e":True, "top":{"disp-value":"task6", "labels":["top"]}}
 	return render("manager-template.html", project_name=project, project=tasks)
 
 server.run(host="127.0.0.1", port="3000", debug=True)
