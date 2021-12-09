4cb07b0f603103d93d83defa8c067fb436e0baf9
Tristan <73963573+tsj845@users.noreply.github.com>
2021-12-09 15:38:49 +0000

    redirect

--- server.py
+++ server.py
@@ -11,6 +11,8 @@ def projects():
 
 @server.route("/projects/<project>")
 def project(project):
+	if (project == ""):
+		return server.redirect("/")
     tasks = {"low":[{"disp-value":"task1"}, {"disp-value":"task2"}, {"disp-value":"task3"}], "med":[{"disp-value":"task4"}], "high":[{"disp-value":"task5"}], "top-e":True, "top":{"disp-value":"task6"}}
     return render("manager-template.html", project_name=project, project=tasks)
 
