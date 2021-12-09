3362e20c36220cdb99a07ff2d253cf0aa55fc274
Tristan <73963573+tsj845@users.noreply.github.com>
2021-12-09 15:39:47 +0000

    fixed indent compatability

--- server.py
+++ server.py
@@ -6,14 +6,14 @@ server.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @server.route("/")
 def projects():
-    projects = ["project1", "project2", "project3"]
-    return render("projects-template.html", projects=projects)
+	projects = ["project1", "project2", "project3"]
+	return render("projects-template.html", projects=projects)
 
 @server.route("/projects/<project>")
 def project(project):
 	if (project == ""):
 		return server.redirect("/")
-    tasks = {"low":[{"disp-value":"task1"}, {"disp-value":"task2"}, {"disp-value":"task3"}], "med":[{"disp-value":"task4"}], "high":[{"disp-value":"task5"}], "top-e":True, "top":{"disp-value":"task6"}}
-    return render("manager-template.html", project_name=project, project=tasks)
+	tasks = {"low":[{"disp-value":"task1"}, {"disp-value":"task2"}, {"disp-value":"task3"}], "med":[{"disp-value":"task4"}], "high":[{"disp-value":"task5"}], "top-e":True, "top":{"disp-value":"task6"}}
+	return render("manager-template.html", project_name=project, project=tasks)
 
 server.run(host="127.0.0.1", port="3000", debug=True)
