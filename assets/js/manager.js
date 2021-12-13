609936a9e5d7052eebc7bcc562e9af69bd79cd3c
Tristan <tschartman41@gmail.com>
2021-12-13 16:13:37 -0500

    began switch

--- assets/js/manager.js
+++ assets/js/manager.js
@@ -1,131 +1,72 @@
 let socket = io();
-let successid = -1;
-let updat = {};
 const pname = document.getElementById("proj-name");
-let origin = pname.textContent;
-const mtn = document.getElementById("manage-name");
-const nc = document.getElementById("mtn-name");
-const mt = document.getElementById("manage-task");
-const tn = document.getElementById("mtk-name");
-const tl = document.getElementById("mtk-labels");
-const topp = document.getElementById("pri-top");
-const ntd = document.getElementById("new-task-dialog");
-const ntdname = document.getElementById("ntd-disp-value");
-const ntdprio = document.getElementById("ntd-priority");
+const tasklist = document.getElementById("task-list");
 const menu = document.getElementById("menu");
-let current_task = null;
-function mkTask (data) {
-    const l = document.createElement("li");
-    const task = {"disp-value":data.name,priority:data.priority,"labels":[],"subtasks":[]};
-    l.textContent = data.name;
-    l.className = "task";
-    l.setAttribute("onclick", "showmanage("+task+")");
-    return l;
+let origin = pname.textContent;
+let tasks = null;
+
+let booted = false;
+
+function makeTask (data) {
+    const cont = document.createElement("div");
+    cont.className = "task";
+    const name = document.createElement("div");
+    name.className = "task-name";
+    const nametext = document.createElement("span");
+    nametext.textContent = data.name;
+    name.appendChild(nametext);
+    const priority = document.createElement("div");
+    priority.className = "task-priority";
+    const prioritytext = document.createElement("span");
+    prioritytext.textContent = data.priority;
+    priority.appendChild(prioritytext);
+    cont.appendChild(name);
+    cont.appendChild(priority);
+    tasklist.appendChild(cont);
 }
-function handleupdate () {
-    switch (successid) {
-        case -1:
-            break;
-        case 0:
-            origin = updat.name;
-            pname.textContent = origin;
-            window.location.pathname = "/projects/"+origin;
-            break;
-        case 1:
-            console.log("remove success");
-            if (updat.priority === "top") {
-                topp.className = "";
-                topp.setAttribute("onclick", "");
-                topp.textContent = "no top priority";
-            } else {
-                document.getElementById("pri-"+updat.priority).removeChild(document.getElementById("pri-"+updat.priority).children[updat.index]);
-            }
-            break;
-        case 2:
-            document.getElementById("pri-"+updat.priority).appendChild(mkTask(updat));
-            break;
-        default:
-            break;
-    }
-}
-function showmanage (task) {
-    console.log(task, typeof task);
-    current_task = task;
-    tn.textContent = task["disp-value"];
-    tl.replaceChildren();
-    for (let i = 0; i < task.labels.length; i ++) {
-        const li = document.createElement("li");
-        li.textContent = task.labels[i];
-        tl.appendChild(li);
-    }
-    mt.showModal();
-}
-function removetask () {
-    mt.close();
-    if (current_task === null) {
+
+function bootrender () {
+    if (!booted) {
         return;
     }
-    successid = 1;
-    socket.emit("remove-task", {"origin":origin, "task-priority":current_task.priority, "task-value":current_task["disp-value"]});
-}
-function applysettings () {
-    mt.close();
-    if (current_task === null) {
-        return;
+    for (let i in tasks) {
+        const task = tasks[i];
+        makeTask(task);
     }
 }
-function managename () {
-    nc.value = pname.textContent;
-    mtn.showModal();
-}
-function applyname () {
-    const name = nc.value;
-    // pname.textContent = name;
-    mtn.close();
-    successid = 0;
-    updat = {name:name};
-    socket.emit("update", {"update-target":"name", "origin":origin, "update-value":name});
-}
-function newtask () {
-    ntdname.value = "";
-    ntdprio.value = "1";
-    ntd.showModal();
-}
-function addtask () {
-    ntd.close();
-    const name = ntdname.value;
-    const priority = ntdprio.value;
-    successid = 2;
-    updat = {name:name, priority:{"0":"low","1":"med","2":"high"}[priority]};
-    socket.emit("new-task", {"origin":origin, "task-value":name, "task-priority":priority});
-}
-socket.on("connect", () => {
-    console.log("connect");
-    socket.emit("connection", {data:"connection active"});
-});
-socket.on("update-success", () => {
-    console.log("success");
-    handleupdate();
-});
-socket.on("update-failed", (data) => {
-    console.log(data.reason);
-});
-socket.on("remove-failed", (data) => {
-    console.log(data.reason);
-});
-socket.on("remove-success", (data) => {
-    updat = data;
-    handleupdate();
-});
-socket.on("add-failed", (data) => {
-    console.log(data.reason);
-});
-socket.on("add-success", () => {
-    handleupdate();
+
+socket.emit("boot", {project:origin});
+
+socket.on("boot-res", (data) => {
+    if (booted) {
+        window.location.pathname = "/err/1";
+    } else {
+        booted = true;
+        tasks = data.tasks;
+        bootrender();
+    }
 });
 
-socket.on("join-fail", () => {
-    window.location.pathname = "/err/0";
+socket.on("update", (data) => {
+    const originator = data.sender;
+    if (socket.id === originator) {
+        return;
+    }
+    const upid = data.id;
+    switch (upid) {
+        // name change
+        case 0:
+            break;
+        // task property changed
+        case 1:
+            break;
+        // task removed
+        case 2:
+            break;
+        // task added
+        case 3:
+            break;
+    }
 });
 
 let command = false;
--- server.py
+++ server.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask import render_template as render
-from flask_socketio import SocketIO, send, emit
+import flask
+import flask_socketio
+SocketIO = flask_socketio.SocketIO
+send = flask_socketio.send
+emit = flask_socketio.emit
+# print(dir(flask_socketio))
+# from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from werkzeug.exceptions import abort
 
 server = Flask(__name__, static_folder="assets")
 server.config["TEMPLATES_AUTO_RELOAD"] = True
 socketio = SocketIO(server)
 
-def mkTask (value, priority="med", labels=None, subtasks=None):
-	task = {"disp-value":value, "priority":priority, "labels":labels if labels else [], "subtasks":subtasks if subtasks else []}
+def mkTask (value, priority="med", labels=None, subtasks=None, locked=False):
+	task = {"name":value, "priority":priority, "labels":labels if labels else [], "subtasks":subtasks if subtasks else [], "locked":True if locked else False}
 	return task
 
 projects = {"project1":[{"name":"task1", "priority":"low", "labels":[], "subtasks":[], "locked":False}]}
@@ -20,18 +26,18 @@ def projectsf ():
 server.add_url_rule("/", endpoint="index")
 server.add_url_rule("/projects", endpoint="index")
 
-@server.context_processor
-def useful_functions():
-	def task_html(task):
-		if "subtasks" in task:
-			html = "<p class=\'task\'>" + task["disp-value"] + "</p>" + "<ul>"
-			for subtask in task["subtasks"]:
-				html += f"<li>" + task_html(subtask) + "</li>"
-			html += "</ul>"
-			return html
-		else:
-			return "<p class=\'task\'>" + task["disp-value"] + "</p>"
-	return dict(task_html=task_html)
+# @server.context_processor
+# def useful_functions():
+# 	def task_html(task):
+# 		if "subtasks" in task:
+# 			html = "<p class=\'task\'>" + task["name"] + "</p>" + "<ul>"
+# 			for subtask in task["subtasks"]:
+# 				html += f"<li>" + task_html(subtask) + "</li>"
+# 			html += "</ul>"
+# 			return html
+# 		else:
+# 			return "<p class=\'task\'>" + task["name"] + "</p>"
+# 	return dict(task_html=task_html)
 
 @server.route("/projects/<project>")
 def projectf (project):
@@ -57,111 +63,17 @@ def handle_internal_error (e):
 # server.register_error_handler(404, handle_bad_request)
 
 @socketio.on("connection")
-def hand_connect (data):
+def hand_connect ():
+	pass
+
+@socketio.on("boot")
+def boot_client (data):
 	print(data)
-
-def _val_updat (data):
-	keys = data.keys()
-	if ("origin" not in keys):
-		return True
-	if ("update-target" not in keys):
-		return True
-	if ("update-value" not in keys):
-		return True
-	return False
-
-@socketio.on("update")
-def hand_update (data):
-	print(data)
-	if (_val_updat(data)):
-		emit("update-failed", {"reason":"invalid data"})
-		return
-	origin = data["origin"]
-	target = data["update-target"]
-	if (target == "name"):
-		if (origin not in projects.keys()):
-			emit("update-failed", {"reason":"invalid origin"})
-			return
-		projects[data["update-value"]] = projects[origin]
-		projects.pop(origin)
-		emit("update-success")
-	else:
-		emit("update-failed", {"reason":"update target not supported"})
-
-def _val_rmdat (data):
-	keys = data.keys()
-	if ("origin" not in keys):
-		return True
-	if ("task-priority" not in keys):
-		return True
-	return False
-
-def _gtaskindex (lst, value):
-	for i in range(len(lst)):
-		if (lst[i]["disp-value"] == value):
-			return i
-	return -1
-
-@socketio.on("remove-task")
-def remtask (data):
-	if (_val_rmdat(data)):
-		emit("remove-failed", {"reason":"invalid data"})
-		return
-	origin = data["origin"]
-	priority = data["task-priority"]
-	if (origin not in projects.keys()):
-		emit("remove-failed", {"reason":"invalid origin"})
-		return
-	if (priority == "top"):
-		projects[origin]["top-e"] = False
-		emit("remove-success", {"index":0, "priority":"top"})
-		return
-	value = data["task-value"]
-	if (priority not in ("low", "med", "high")):
-		emit("remove-failed", {"reason":"invalid task priority"})
-		return
-	tasklst = projects[origin][priority]
-	ind = _gtaskindex(tasklst, value)
-	if (ind == -1):
-		emit("remove-failed", {"reason":"task does not exist"})
-		return
-	tasklst.pop(ind)
-	projects[origin][priority] = tasklst
-	emit("remove-success", {"index":ind, "priority":priority})
-
-def _val_addat (data):
-	keys = data.keys()
-	if ("origin" not in keys):
-		return True
-	if ("task-priority" not in keys):
-		return True
-	if ("task-value" not in keys):
-		return True
-	return False
-
-@socketio.on("new-task")
-def addtask (data):
-	if (_val_addat(data)):
-		emit("add-failed", {"reason":"invalid data"})
-		return
-	origin = data["origin"]
-	name = data["task-value"]
-	priority = data["task-priority"]
-	if (origin not in projects.keys()):
-		emit("add-failed", {"reason":"invalid project name"})
-		return
-	if (priority not in "012"):
-		emit("add-failed", {"reason":"invalid priority"})
-		return
-	priority = {"0":"low", "1":"med", "2":"high"}[priority]
-	l = projects[origin][priority]
-	if (name in l):
-		emit("add-failed", {"reason":"task already exists"})
-		return
-	task = mkTask(name, priority)
-	l.append(task)
-	projects[origin][priority] = l
-	emit("add-success")
+	flask_socketio.join_room(data["project"])
+	print(flask_socketio.rooms())
+	print(str(flask_socketio.has_request_context()), "boot")
+	print(dir(flask_socketio.flask.request), "request")
+	print(flask_socketio.flask.request.sid)
 
 # server
 socketio.run(server, host="127.0.0.1", port="3000", debug=True)
